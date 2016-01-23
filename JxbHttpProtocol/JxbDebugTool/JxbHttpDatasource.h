717e1c114d4da245241bf7d1f6b0c5098f9446a2
PeterJin <i@jxb.name>
2016-01-23 15:51:51 +0800

    支持NSURLSession

--- JxbHttpProtocol/JxbDebugTool/JxbHttpDatasource.h
+++ JxbHttpProtocol/JxbDebugTool/JxbHttpDatasource.h
@@ -10,6 +10,7 @@
 
 
 @interface JxbHttpModel : NSObject
+@property (nonatomic,copy)NSString  *requestId;
 @property (nonatomic,copy)NSURL     *url;
 @property (nonatomic,copy)NSString  *method;
 @property (nonatomic,copy)NSString  *requestBody;
@@ -23,6 +24,7 @@
 @interface JxbHttpDatasource : NSObject
 
 @property (nonatomic,strong,readonly) NSMutableArray    *httpArray;
+@property (nonatomic,strong,readonly) NSMutableArray    *arrRequest;
 
 + (instancetype)shareInstance;
 /**
@@ -36,4 +38,13 @@
  *  清空
  */
 - (void)clear;
+
+/**
+ *  解析
+ *
+ *  @param data
+ *
+ *  @return 
+ */
++ (NSString *)prettyJSONStringFromData:(NSData *)data;
 @end
--- JxbHttpProtocol/JxbDebugTool/JxbHttpDatasource.m
+++ JxbHttpProtocol/JxbDebugTool/JxbHttpDatasource.m
@@ -26,6 +26,7 @@
     self = [super init];
     if (self) {
         _httpArray = [NSMutableArray array];
+        _arrRequest = [NSMutableArray array];
     }
     return self;
 }
@@ -33,6 +34,12 @@
 - (void)addHttpRequset:(JxbHttpModel*)model {
     @synchronized(self.httpArray) {
         [self.httpArray insertObject:model atIndex:0];
+        
+    }
+    @synchronized(self.arrRequest) {
+        if (model.requestId&& model.requestId.length > 0) {
+            [self.arrRequest addObject:model.requestId];
+        }
     }
 }
 
@@ -40,5 +47,26 @@
     @synchronized(self.httpArray) {
         [self.httpArray removeAllObjects];
     }
+    @synchronized(self.arrRequest) {
+        [self.arrRequest removeAllObjects];
+    }
+}
+
+
+#pragma mark - parse
++ (NSString *)prettyJSONStringFromData:(NSData *)data
+{
+    NSString *prettyString = nil;
+    
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+    if ([NSJSONSerialization isValidJSONObject:jsonObject]) {
+        prettyString = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:jsonObject options:NSJSONWritingPrettyPrinted error:NULL] encoding:NSUTF8StringEncoding];
+        // NSJSONSerialization escapes forward slashes. We want pretty json, so run through and unescape the slashes.
+        prettyString = [prettyString stringByReplacingOccurrencesOfString:@"\\/" withString:@"/"];
+    } else {
+        prettyString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    
+    return prettyString;
 }
 @end
