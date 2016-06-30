f6376e1a546398e1c58b81adf31fcd9736293064
PeterJin <i@jxb.name>
2016-06-30 20:24:54 +0800

    print log

--- JxbHttpProtocol/JxbDebugTool/JxbContentVC.m
+++ JxbHttpProtocol/JxbDebugTool/JxbContentVC.m
@@ -67,6 +67,7 @@
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         weakTxt.text = wSelf.content;
+        NSLog(@"%@",wSelf.content);
     });
 }
 
